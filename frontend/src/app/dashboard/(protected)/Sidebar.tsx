6f4ef273388c5b9b4e352047b9b4825e97fd40df
kasope_johnson <kj@gtco.net>
2026-05-25 18:04:30 +0100

    fix: adds profile elements in sidebar and logout button

--- frontend/src/app/dashboard/(protected)/Sidebar.tsx
+++ frontend/src/app/dashboard/(protected)/Sidebar.tsx
@@ -1,12 +1,18 @@
 'use client'
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarMenuLink, SidebarProvider } from '@/components/ui/sidebar'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuItem, SidebarMenuLink, SidebarProvider } from '@/components/ui/sidebar'
 import Image from 'next/image'
 import AppLogo from '@/assets/logo/app_logo.svg?url'
 import Link from 'next/link'
 import { isGroup, useMenu } from '@/hooks/use-menu'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { useAuth } from '@/context-providers/AuthProvider'
+import { getInitials } from '@/lib/utils/format'
+import { Button } from '@/components/ui/button'
+import { LogOutIcon } from 'lucide-react'
 
 export default function AppSidebar() {
   const { menu = [], flatMenu = [] } = useMenu()
+  const { user, logout } = useAuth()
   return (
     <SidebarProvider>
       <Sidebar>
@@ -44,6 +50,22 @@ export default function AppSidebar() {
             }
           </SidebarMenu>
         </SidebarContent>
+        <SidebarFooter className="flex-row gap-2">
+          <div className="w-full flex gap-2 items-center">
+            <Avatar className="size-8">
+              <AvatarImage src={user?.user_metadata.avatar_url} alt={user?.user_metadata.first_name} />
+              <AvatarFallback className="uppercase">{getInitials(`${user?.user_metadata.first_name} ${user?.user_metadata.last_name}`)}</AvatarFallback>
+            </Avatar>
+            <span className="text-sm text-contrast-foreground font-bold capitalize">
+              {user?.user_metadata.first_name}
+              {' '}
+              {user?.user_metadata.last_name}
+            </span>
+          </div>
+          <Button variant="destructive" size="icon" onClick={logout}>
+            <LogOutIcon />
+          </Button>
+        </SidebarFooter>
       </Sidebar>
     </SidebarProvider>
   )
