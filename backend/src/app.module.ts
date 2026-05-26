0d4241648e3e1e5160f616afed635bc4b3c99ccb
kasope_johnson <kj@gtco.net>
2026-05-26 01:15:23 +0100

    fix: configures app for deployment environment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { UploadModule } from './modules/upload/upload.module';
 import { PostModule } from './modules/post/post.module';
 import { PermissionModule } from './modules/permission/permission.module';
 
+const isProdApp = process.env.ENVIRONMENT === 'production';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -26,9 +27,10 @@ import { PermissionModule } from './modules/permission/permission.module';
         SUPABASE_URL: Joi.string().required(),
         SUPABASE_PUBLIC_KEY: Joi.string().required(),
         SUPABASE_SECRET_KEY: Joi.string().required(),
-        DB_USER: Joi.string().required(),
-        DB_PASSWD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
+        DB_USER: isProdApp ? Joi.string() : Joi.string().required(),
+        DB_PASSWD: isProdApp ? Joi.string() : Joi.string().required(),
+        DB_NAME: isProdApp ? Joi.string() : Joi.string().required(),
+        DB_URL: !isProdApp ? Joi.string() : Joi.string().required(),
         POSTHOG_API_KEY: Joi.string().required(),
       }),
       validationOptions: {
@@ -42,14 +44,19 @@ import { PermissionModule } from './modules/permission/permission.module';
       useFactory() {
         return {
           type: 'postgres',
-          host: 'localhost',
-          port: Number(process.env.DB_PORT),
-          username: process.env.DB_USER,
-          password: process.env.DB_PASSWD,
-          database: process.env.DB_NAME,
+          ...(isProdApp
+            ? {
+                url: process.env.DB_URL,
+              }
+            : {
+                host: 'localhost',
+                port: Number(process.env.DB_PORT),
+                username: process.env.DB_USER,
+                password: process.env.DB_PASSWD,
+                database: process.env.DB_NAME,
+              }),
           autoLoadEntities: true,
-          synchronize: true,
-          // synchronize: process.env.ENVIRONMENT === 'development',
+          synchronize: process.env.ENVIRONMENT === 'development',
         };
       },
     }),
