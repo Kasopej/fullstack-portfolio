25f259760cd808e928a0429127a2312c9e1b3980
kasope_johnson <kj@gtco.net>
2026-05-26 01:46:02 +0100

    ffix: backend - update cors whitelist

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,7 +17,10 @@ async function bootstrap() {
   app.use(express.json({ limit: '10mb' }));
   app.use(express.urlencoded({ limit: '10mb', extended: true }));
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [
+      'http://localhost:3000',
+      'https://fullstack-portfolio-sable.vercel.app/',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
